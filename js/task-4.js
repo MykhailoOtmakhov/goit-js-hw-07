243b3938c3ef1758515fada0a39616242218c5de
MykhailoOtmakhov <65648953+MykhailoOtmakhov@users.noreply.github.com>
2020-10-15 16:27:44 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,9 @@
 let counterResult = 0;
 let counterValue = document.querySelector('span');
-const decrement =()=> {--counterValue.textContent;};
+const decrement =()=> {counterResult = --counterValue.textContent;};
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 decrementBtn.addEventListener('click', decrement);
-const increment = () => {++counterValue.textContent;};
+const increment = () => {counterResult = ++counterValue.textContent;};
 const incrementBtn = document.querySelector('[data-action="increment"]');
 incrementBtn.addEventListener('click', increment);
 
