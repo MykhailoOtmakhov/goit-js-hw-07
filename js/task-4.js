12aa8cefbdb69aaae0a4927575e6f34ba8921e40
MykhailoOtmakhov <65648953+MykhailoOtmakhov@users.noreply.github.com>
2020-10-18 21:34:07 +0300

    Исправил задача 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,9 @@
 let counterResult = 0;
 let counterValue = document.querySelector('span');
-const decrement =()=> {counterResult = --counterValue.textContent;};
+const decrement =()=> {counterValue.textContent = --counterResult;};
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 decrementBtn.addEventListener('click', decrement);
-const increment = () => {counterResult = ++counterValue.textContent;};
+const increment = () => {counterValue.textContent = ++counterResult;};
 const incrementBtn = document.querySelector('[data-action="increment"]');
 incrementBtn.addEventListener('click', increment);
 
